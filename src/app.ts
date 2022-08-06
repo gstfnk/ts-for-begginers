16c5b43ea8121eb594442a02de97b60fb5ff13d2
gstfnk <gabrysia.stefaniak@gmail.com>
2022-08-06 13:56:37 +0200

    backup 06.08

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+// let uni1: Universal = true;
+let uni1: Universal = 2;
